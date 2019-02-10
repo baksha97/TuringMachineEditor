6521fb34543ef132464aadd7bdc064215f640940
Travis <baksha97@gmail.com>
2019-02-10 17:23:42 -0500

    cmt

--- src/fx/Main.java
+++ src/fx/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("editor.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Turing Machine Editor v2.10.2019.1723");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
